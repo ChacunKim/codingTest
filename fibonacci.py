c4f323e9e821595351c988db24aeaf0e743a0df7
ChacunKim <cocodata12@gmail.com>
2023-01-04 16:38:25 +0900

    codingTest Study 230104/antEscape, tashu

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+num = int(input())
+
+def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fibonacci(num-1) + fibonacci(num-2)
+
+
+print(fibonacci(num))
